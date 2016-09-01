a0052bbce54e7c6fad3f274a11b65c9c6f67e608
Geiger <geigereitan@gmail.com>
2016-09-01 22:44:34 +0300

    allows multiple run to receive the number of desired times

--- bin/multiple_run.sh
+++ bin/multiple_run.sh
@@ -1,14 +1,20 @@
-#! /bin/bash
+#! /usr/bin/time /bin/bash
 
 # make sure the file is right
 headerline="handoff_fail_rate,initiated_fail_rate,channel_utilization,handoff_avg_pending,initiated_avg_pending"
 csvfile="../cellnet_simulation.csv"
+if [ "$1" == "" ]
+then
+    times=10
+else
+    times=$1
+fi
 if ! [ -f $csvfile ] || [ "`head -1 $csvfile`" != $headerline ]
 then
     echo $headerline > $csvfile
 fi
 i=0
-while [ $i -lt 10 ]
+while [ $i -lt $times ]
 do
     /usr/bin/python2.7 ./cellnet_simulation.py >> $csvfile
     i=$(($i+1))
